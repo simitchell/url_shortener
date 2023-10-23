cc2a820dbd13f8641c5590d5576758cb88d6d37f
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-23 14:42:42 -0400

    created models

--- backend/backend_project/backend_app/models.py
+++ backend/backend_project/backend_app/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class UrlModel(models.Model):
+class UserModel(models.Model):
     username = models.CharField(null=True)
     password = models.CharField(null=True)
     token = models.CharField(null=True)
 
     def __str__(self) -> str:
         return self.username
+    
+class UrlModel(models.Model):
+    user_id = models.IntegerField(null=True)
+    long_url = models.CharField(null=True)
+    short_url = models.CharField(null=True)
+
+    def __str__(self) -> str:
+        return self.short_url
