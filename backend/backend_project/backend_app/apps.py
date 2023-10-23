b3b36c247142691847bcb05c13b8abb710cfda82
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-23 14:01:02 -0400

    fixes on the backend.  settings.py

--- backend/backend_project/backend_app/apps.py
+++ backend/backend_project/backend_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BackendAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend_app'
+    name = 'backend_project.backend_app'
