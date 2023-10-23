106bd63aa738dde43fb649f1b9716c351191c57c
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-23 14:34:27 -0400

    changes to backend

--- backend/backend_project/backend_app/models.py
+++ backend/backend_project/backend_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UrlModel(models.Model):
+    username = models.CharField(null=True)
+    password = models.CharField(null=True)
+    token = models.CharField(null=True)
+
+    def __str__(self) -> str:
+        return self.username
